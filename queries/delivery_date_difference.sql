4d60de5d23a7e46b78a43d994f36784edf2bd8f8
rreys <roger.reyesm97@gmail.com>
2024-02-02 22:31:46 -0500

    query delivery_date_difference

--- queries/delivery_date_difference.sql
+++ queries/delivery_date_difference.sql
@@ -8,10 +8,10 @@
 -- 2. You can use the CAST function to convert a number to an integer.
 -- 3. You can use the STRFTIME function to convert a order_delivered_customer_date to a string removing hours, minutes and seconds.
 -- 4. order_status == 'delivered' AND order_delivered_customer_date IS NOT NULL
-SELECT geolocation_state AS State, ROUND(AVG(JULIANDAY(order_estimated_delivery_date) - JULIANDAY(order_delivered_customer_date))) AS Delivery_Difference
+SELECT oc.customer_state AS State, 
+CAST(AVG(JULIANDAY(STRFTIME('%Y-%m-%d', order_estimated_delivery_date)) - JULIANDAY(STRFTIME('%Y-%m-%d', order_delivered_customer_date))) AS INTEGER) AS Delivery_Difference
 FROM olist_customers AS oc 
 JOIN olist_orders AS oo ON oc.customer_id = oo.customer_id
-JOIN olist_geolocation AS og ON oc.customer_zip_code_prefix = og.geolocation_zip_code_prefix
 WHERE oo.order_status = 'delivered' AND oo.order_delivered_customer_date IS NOT NULL
-GROUP BY geolocation_state
+GROUP BY oc.customer_state
 ORDER BY Delivery_Difference ASC
